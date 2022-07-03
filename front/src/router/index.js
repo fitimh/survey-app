cf2c75660592e5fc8c669036fb34111fb5840a45
Fitim Hoti <fitim.hoti100@gmail.com>
2022-07-04 01:33:51 +0200

    Fixes some error on dashboard

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,36 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from '../views/Dashboard.vue'
-import DefaultLayout from "../components/DefaultLayout.vue"
-import Login from "../views/Login.vue"
-import register from "../views/Register.vue"
+import {createRouter, createWebHistory} from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import Login from "../views/Login.vue";
+import register from "../views/Register.vue";
 
 const routes = [
-  {
-    path: "/dashboard",
-    name: 'dashboard',
-    component: Dashboard,
-  },
-  {
-    path: "/",
-    name: 'home',
-    component: DefaultLayout,
-
-
-  },
-  {
-    path: "/login",
-    name: 'login',
-    component: Login,
-  },
-  {
-    path: "/register",
-    name: 'register',
-    component: register,
-  },
+   {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard,
+   },
+   {
+      path: "/",
+      name: "home",
+      component: DefaultLayout,
+   },
+   {
+      path: "/login",
+      name: "login",
+      component: Login,
+   },
+   {
+      path: "/register",
+      name: "register",
+      component: register,
+   },
 ];
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+   history: createWebHistory(),
+   routes,
 });
 
 export default router;
