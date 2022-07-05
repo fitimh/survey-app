f263fea5f0ca02b1239161f5eed3c373db239ea3
Fitin Hoti <fitim.hoti100@gmail.com>
2022-07-05 10:02:30 +0200

    Fixes register

--- back/app/Models/Survey.php
+++ back/app/Models/Survey.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     use HasFactory;
+
+    protected $table = 'surveys';
+    protected $fillable = [
+//        'table_nr', 'tables_id'
+    ];
+    protected $hidden = [
+        'created_at', 'updated_at', 'deleted_at'
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
 }
+
--- back/routes/api.php
+++ back/routes/api.php
@@ -19,4 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [AuthController::class], 'register');
+
+Route::prefix('auth')->group(function() {
+    Route::post('register', 'AuthController@register');
+});
